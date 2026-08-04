b1cf0f579976911aaad44b034f7e48ed37bd6b24
MadaiMonrroy <madaimonrroy2810@gmail.com>
2026-08-03 20:24:25 -0400

    mensaje del form

--- parcial1/script.js
+++ parcial1/script.js
@@ -1,2 +1,7 @@
 const boton = document.querySelector("#btn-confirmar");
 const mensaje = document.querySelector("#mensaje");
+
+function mostrarMensaje() {
+  mensaje.textContent = "Turno recibido - te atiende Madai Alejandra Monrroy Vega";
+  mensaje.classList.remove("oculto");
+}
