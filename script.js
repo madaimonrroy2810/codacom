f22732e8f01acf1ff6364501423dfdf618ef8454
madaimonrroy2810 <madaialejandra.monrroy@uab.edu.bo>
2026-08-04 23:49:01 -0400

    update en js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const boton = document.querySelector("#btn-tema");
 const icono = document.querySelector("#icono-tema");
 
@@ -5,10 +7,47 @@ boton.addEventListener("click", function () {
 
     document.body.classList.toggle("oscuro");
 
-    if(document.body.classList.contains("oscuro")){
+    if (document.body.classList.contains("oscuro")) {
         icono.className = "fa-solid fa-sun";
-    }else{
+    } else {
         icono.className = "fa-solid fa-moon";
     }
 
 });
+
+
+
+const formulario = document.querySelector("#form-contacto");
+const aviso = document.querySelector("#error-formulario");
+
+function revisarFormulario(event) {
+
+    event.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+
+    if (nombre === "") {
+
+        aviso.textContent = "Debes escribir tu nombre.";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+
+    } else if (email.includes("@") === false) {
+
+        aviso.textContent = "El correo no es válido.";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+
+    } else {
+
+        aviso.textContent = "¡Mensaje enviado correctamente!";
+        aviso.classList.add("exito");
+        aviso.classList.remove("error");
+
+    }
+
+}
+
+formulario.addEventListener("submit", revisarFormulario);
+
