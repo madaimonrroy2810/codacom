838f693dca9158be958d4e3a4d06553fbdfdbe3e
MadaiMonrroy <madaimonrroy2810@gmail.com>
2026-08-17 23:38:05 -0400

    con base de datos

--- codacom-laravel/routes/web.php
+++ codacom-laravel/routes/web.php
@@ -3,13 +3,7 @@
 use App\Http\Controllers\ContactoController;
 use Illuminate\Support\Facades\Route;
  
-/*
-|--------------------------------------------------------------------------
-| Rutas de CodaCom
-|--------------------------------------------------------------------------
-| Sin base de datos por ahora: solo vistas Blade + un formulario que se
-| valida y se procesa con un Controller (reemplaza al viejo procesar.php).
-*/
+
  
 Route::get('/', function () {
     return view('inicio');
